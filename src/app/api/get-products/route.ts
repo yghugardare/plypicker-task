f2857fd9db4ae849b8e3f3dcac24e4e531bc04f2
Yash Ghugardare <yghugardare6@gmail.com>
2024-08-29 21:41:26 +0530

    header no cache

--- src/app/api/get-products/route.ts
+++ src/app/api/get-products/route.ts
@@ -13,6 +13,11 @@ export async function GET() {
       },
       {
         status: 200,
+        headers: {
+          'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          'Pragma': 'no-cache',
+          'Expires': '0',
+        },
       }
     );
   } catch (error) {
